4e528d87b2e25468c0bb04c5f25a52b0496d0154
alvaropalacios-gnoss <60385160+alvaropalacios-gnoss@users.noreply.github.com>
2022-10-03 15:14:42 +0200

    Cambio carpeta CommonsEDMA.Load

---
+++ src/Hercules.CommonsEDMA.Load/Hercules.CommonsEDMA.Load/Models/CVN/Thesaurus.cs
@@ -0,0 +1,171 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hercules.CommonsEDMA.Load.Models.CVN
+{
+
+    // NOTA: El código generado puede requerir, como mínimo, .NET Framework 4.5 o .NET Core/Standard 2.0.
+    /// <remarks/>
+    [System.SerializableAttribute()]
+    [System.ComponentModel.DesignerCategoryAttribute("code")]
+    [System.Xml.Serialization.XmlTypeAttribute(AnonymousType = true, Namespace = "http://cv.normalizado.org/thesaurus")]
+    [System.Xml.Serialization.XmlRootAttribute(Namespace = "http://cv.normalizado.org/thesaurus", IsNullable = false)]
+    public partial class Thesaurus
+    {
+
+        private item[] itemField;
+
+        /// <remarks/>
+        [System.Xml.Serialization.XmlElementAttribute("item", Namespace = "")]
+        public item[] item
+        {
+            get
+            {
+                return this.itemField;
+            }
+            set
+            {
+                this.itemField = value;
+            }
+        }
+    }
+
+    /// <remarks/>
+    [System.SerializableAttribute()]
+    [System.ComponentModel.DesignerCategoryAttribute("code")]
+    [System.Xml.Serialization.XmlTypeAttribute(AnonymousType = true)]
+    [System.Xml.Serialization.XmlRootAttribute(Namespace = "", IsNullable = false)]
+    public partial class item
+    {
+
+        private string itemIdField;
+
+        private byte itemOrderField;
+
+        private string itemAncestorIdField;
+
+        private itemItemDescription[] itemDescriptionField;
+
+        /// <remarks/>
+        [System.Xml.Serialization.XmlElementAttribute(DataType = "integer")]
+        public string itemId
+        {
+            get
+            {
+                return this.itemIdField;
+            }
+            set
+            {
+                this.itemIdField = value;
+            }
+        }
+
+        /// <remarks/>
+        public byte itemOrder
+        {
+            get
+            {
+                return this.itemOrderField;
+            }
+            set
+            {
+                this.itemOrderField = value;
+            }
+        }
+
+        /// <remarks/>
+        [System.Xml.Serialization.XmlElementAttribute(DataType = "integer")]
+        public string itemAncestorId
+        {
+            get
+            {
+                return this.itemAncestorIdField;
+            }
+            set
+            {
+                this.itemAncestorIdField = value;
+            }
+        }
+
+        /// <remarks/>
+        [System.Xml.Serialization.XmlElementAttribute("itemDescription")]
+        public itemItemDescription[] itemDescription
+        {
+            get
+            {
+                return this.itemDescriptionField;
+            }
+            set
+            {
+                this.itemDescriptionField = value;
+            }
+        }
+    }
+
+    /// <remarks/>
+    [System.SerializableAttribute()]
+    [System.ComponentModel.DesignerCategoryAttribute("code")]
+    [System.Xml.Serialization.XmlTypeAttribute(AnonymousType = true)]
+    public partial class itemItemDescription
+    {
+
+        private itemItemDescriptionNameDetail nameDetailField;
+
+        /// <remarks/>
+        public itemItemDescriptionNameDetail NameDetail
+        {
+            get
+            {
+                return this.nameDetailField;
+            }
+            set
+            {
+                this.nameDetailField = value;
+            }
+        }
+    }
+
+    /// <remarks/>
+    [System.SerializableAttribute()]
+    [System.ComponentModel.DesignerCategoryAttribute("code")]
+    [System.Xml.Serialization.XmlTypeAttribute(AnonymousType = true)]
+    public partial class itemItemDescriptionNameDetail
+    {
+
+        private string nameField;
+
+        private string langField;
+
+        /// <remarks/>
+        public string Name
+        {
+            get
+            {
+                return this.nameField;
+            }
+            set
+            {
+                this.nameField = value;
+            }
+        }
+
+        /// <remarks/>
+        [System.Xml.Serialization.XmlAttributeAttribute()]
+        public string lang
+        {
+            get
+            {
+                return this.langField;
+            }
+            set
+            {
+                this.langField = value;
+            }
+        }
+    }
+
+
+}
