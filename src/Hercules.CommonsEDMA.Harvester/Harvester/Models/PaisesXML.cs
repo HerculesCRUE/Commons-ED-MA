eba736a5d619318ff51a4bd09fb0b62dbdf70bc9
Adrián <88077103+AdrianSaavedra-GNOSS@users.noreply.github.com>
2022-10-18 12:28:09 +0200

    Update Harvester
    - Agregado fichero países.
    - Limpieza de código en clase Persona.
    - Agregado comentarios.
    - Corrección de errores.

---
+++ src/Hercules.CommonsEDMA.Harvester/Harvester/Models/PaisesXML.cs
@@ -0,0 +1,28 @@
+﻿namespace QuickType
+{
+    using Newtonsoft.Json;
+
+    public partial class MainPaises
+    {
+        [JsonProperty("PAISES")]
+        public Paises Paises { get; set; }
+    }
+
+    public partial class Paises
+    {
+        [JsonProperty("DATA_RECORD")]
+        public DataRecord[] DataRecord { get; set; }
+    }
+
+    public partial class DataRecord
+    {
+        [JsonProperty("PAI_CODIGO")]
+        public string PaiCodigo { get; set; }
+
+        [JsonProperty("PAI_NOMBRE")]
+        public string PaiNombre { get; set; }
+
+        [JsonProperty("PAI_ISO31661_A3")]
+        public string PaiIso31661A3 { get; set; }
+    }
+}
