acbe4c699dd9197b4669675e2a8e997fa76e5f63
ManuelGNOSS <manuelboillos@gnoss.com>
2022-10-20 12:29:02 +0200

    Update importacioncv.js

--- Web/Estilos/theme/importacioncv.js
+++ Web/Estilos/theme/importacioncv.js
@@ -206,6 +206,7 @@ var importarCVN = {
 				$('#informacionImportacion').show();
 				$('#titleMascaraBlanca').remove();
 				$('#workMascaraBlanca').remove();
+				$('#subworkMascaraBlanca').remove();
 				for(var i=0;i<7;i++){
 					var id = 'x' + RandomGuid();
 					var dropdowns = '';
@@ -292,6 +293,7 @@ var importarCVN = {
 				clearInterval(intervalStatus);				
 				$('#titleMascaraBlanca').remove();
 				$('#workMascaraBlanca').remove();
+				$('#subworkMascaraBlanca').remove();
 				window.removeEventListener('beforeunload', preventBeforeUnload);
 				
 				var msg = '';
