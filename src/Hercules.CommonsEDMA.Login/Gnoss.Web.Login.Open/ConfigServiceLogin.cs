60a49b79fd7c36c93521d973f0fbcb9d0e7e4ea3
alvaropalacios-gnoss <60385160+alvaropalacios-gnoss@users.noreply.github.com>
2022-10-21 11:22:23 +0200

    Update ConfigServiceLogin.cs

--- src/Hercules.CommonsEDMA.Login/Gnoss.Web.Login.Open/ConfigServiceLogin.cs
+++ src/Hercules.CommonsEDMA.Login/Gnoss.Web.Login.Open/ConfigServiceLogin.cs
@@ -11,6 +11,10 @@ namespace Gnoss.Web.Login.Open
     {
         private string urlServiceInDomain { get; set; }
         private string claimMail { get; set; }
+        private string claimGroups { get; set; }        
+        private string groupAdminIndicadores { get; set; }
+        private string groupAdmin { get; set; }
+        private string groupGestorOtri { get; set; }
 
         private IConfiguration _configuration { get; set; }
 
@@ -57,6 +61,86 @@ namespace Gnoss.Web.Login.Open
                 claimMail = connectionString;
             }
             return claimMail;
+        }
+
+        public string GetClaimGroups()
+        {
+            if (string.IsNullOrEmpty(claimGroups))
+            {
+                string connectionString = string.Empty;
+                IDictionary environmentVariables = Environment.GetEnvironmentVariables();
+                if (environmentVariables.Contains("claimGroups"))
+                {
+                    connectionString = environmentVariables["claimGroups"] as string;
+                }
+                else
+                {
+                    connectionString = _configuration["claimGroups"];
+                }
+
+                claimGroups = connectionString;
+            }
+            return claimGroups;
+        }
+
+        public string GetGroupAdminIndicadores()
+        {
+            if (string.IsNullOrEmpty(groupAdminIndicadores))
+            {
+                string connectionString = string.Empty;
+                IDictionary environmentVariables = Environment.GetEnvironmentVariables();
+                if (environmentVariables.Contains("groupAdminIndicadores"))
+                {
+                    connectionString = environmentVariables["groupAdminIndicadores"] as string;
+                }
+                else
+                {
+                    connectionString = _configuration["groupAdminIndicadores"];
+                }
+
+                groupAdminIndicadores = connectionString;
+            }
+            return groupAdminIndicadores;
+        }
+
+        public string GetGroupAdmin()
+        {
+            if (string.IsNullOrEmpty(groupAdmin))
+            {
+                string connectionString = string.Empty;
+                IDictionary environmentVariables = Environment.GetEnvironmentVariables();
+                if (environmentVariables.Contains("groupAdmin"))
+                {
+                    connectionString = environmentVariables["groupAdmin"] as string;
+                }
+                else
+                {
+                    connectionString = _configuration["groupAdmin"];
+                }
+
+                groupAdmin = connectionString;
+            }
+            return groupAdmin;
+        }
+
+        public string GetGroupGestorOtri()
+        {
+            if (string.IsNullOrEmpty(groupGestorOtri))
+            {
+                string connectionString = string.Empty;
+                IDictionary environmentVariables = Environment.GetEnvironmentVariables();
+                if (environmentVariables.Contains("groupGestorOtri"))
+                {
+                    connectionString = environmentVariables["groupGestorOtri"] as string;
+                }
+                else
+                {
+                    connectionString = _configuration["groupGestorOtri"];
+                }
+
+                groupGestorOtri = connectionString;
+            }
+            return groupGestorOtri;
         }
     }
 }
