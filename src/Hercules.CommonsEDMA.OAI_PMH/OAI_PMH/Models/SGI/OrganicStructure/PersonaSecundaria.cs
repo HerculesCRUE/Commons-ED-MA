03e1df8e052f6d54b320be96415ec129937f3848
alvaropalacios-gnoss <60385160+alvaropalacios-gnoss@users.noreply.github.com>
2022-10-21 11:23:31 +0200

    Create PersonaSecundaria.cs

---
+++ src/Hercules.CommonsEDMA.OAI_PMH/OAI_PMH/Models/SGI/OrganicStructure/PersonaSecundaria.cs
@@ -0,0 +1,22 @@
+﻿namespace OAI_PMH.Models.SGI.OrganicStructure
+{
+    public class PersonaSecundaria : SGI_Base
+    {
+        /// <summary>
+        /// Identificador de la persona
+        /// </summary>
+        public string Id { get; set; }
+        /// <summary>
+        /// Nombre de la persona
+        /// </summary>
+        public string Nombre { get; set; }
+        /// <summary>
+        /// Primer apellido
+        /// </summary>
+        public string PrimerApellido { get; set; }
+        /// <summary>
+        /// Segundo apellido
+        /// </summary>
+        public string SegundoApellido { get; set; }
+    }
+}
