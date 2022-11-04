0f049e049fb3d9def7a89feda8cb72f65b49c4fe
alvaropalacios-gnoss <60385160+alvaropalacios-gnoss@users.noreply.github.com>
2022-11-04 09:04:39 +0100

    Update HerculesController.cs

--- src/Hercules.CommonsEDMA.ServicioExterno/Hercules.CommonsEDMA.ServicioExterno/Controllers/HerculesController.cs
+++ src/Hercules.CommonsEDMA.ServicioExterno/Hercules.CommonsEDMA.ServicioExterno/Controllers/HerculesController.cs
@@ -269,23 +269,5 @@ namespace Hercules.CommonsEDMA.ServicioExterno.Controllers
             return Ok(datosCabeceraFichas);
 
         }
-
-        /// <summary>
-        /// Controlador para obtener la información del usuario en la home de ED.
-        /// </summary>
-        /// <param name="pIdGnossUser">Usuario de gnoss.</param>
-        /// <returns>Diccionario con los datos.</returns>
-        [HttpGet("GetInfoHomeEdUser")]
-        public IActionResult GetInfoHomeEdUser(string pIdGnossUser)
-        {
-            if (!Security.CheckUser(new Guid(pIdGnossUser), Request))
-            {
-                return StatusCode(StatusCodes.Status401Unauthorized);
-            }
-            AccionesPersona accionP = new AccionesPersona();
-            Dictionary<string, string> datosUsuario = accionP.GetInfoHomeEdUser(pIdGnossUser);
-
-            return Ok(datosUsuario);
-        }
     }
 }
