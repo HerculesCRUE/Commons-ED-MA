6b83ec93f171d4108c11fa0fda157e00708676ee
PabloDeGroot <pablodegroot@gnoss.com>
2022-11-08 09:08:36 +0100

    Marcar notificaciones como leidas

---
+++ src/Hercules.CommonsEDMA.ServicioExterno/Hercules.CommonsEDMA.ServicioExterno/Controllers/NotificationController.cs
@@ -0,0 +1,26 @@
+﻿using Hercules.CommonsEDMA.ServicioExterno.Controllers.Acciones;
+using Microsoft.AspNetCore.Cors;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Hercules.CommonsEDMA.ServicioExterno.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    [EnableCors("_myAllowSpecificOrigins")]
+    public class NotificationController : ControllerBase
+    {
+
+        [HttpPost("ReadNotifications")]
+        public IActionResult ReadNotifications([FromForm] string[] notificacionesID)
+        {
+            AccionesNotificaciones acciones = new AccionesNotificaciones();
+            foreach (string notificacion in notificacionesID)
+            {
+                acciones.readNotification(notificacion);
+            }
+
+            return Ok();
+        }
+    }
+}
